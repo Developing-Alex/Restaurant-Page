52740136ea3728eac54af74da503ebc3e127c1dd
Alex <alexnowrang@gmail.com>
2024-02-11 17:21:33 -0600

    Add images and create id's for elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { contentLoader, headLoader } from "./page-load";
 import "./style.css";
+import FriedChicken from './gingham-fried-chicken.jpg';
 
 function component(){
   const header = document.querySelector('header');
@@ -8,7 +9,13 @@ function component(){
 
   const content = document.getElementById('content');
 
-  contentLoader().forEach(e => content.appendChild(e))
+  contentLoader().forEach(e => content.appendChild(e));
+
+  const friedChicken = new Image();
+  friedChicken.src = FriedChicken;
+  friedChicken.id = 'friedChicken';
+
+  content.appendChild(friedChicken);
 }
 
 
--- src/page-load.js
+++ src/page-load.js
@@ -12,12 +12,12 @@ const contentLoader = () => {
   const heading = document.createElement('h2');
 
   heading.textContent = 'Best Chicken in America!';
-  //heading.id = 
+  heading.id = 'heading'
   
   const about = document.createElement('p');
 
-  about.textContent = 'lorem';
-  about.id = 'hello';
+  about.textContent = `Voted Best Chicken in America three times in a row! We at Bishop's Birdhouse strive to bring you the best finger lickin' chicken experience the country has to offer!`;
+  about.id = 'about';
   
   return [heading, about];
 }
