e0baa63d6d4c44398abb6adac802490f9ba76514
Alex <alexnowrang@gmail.com>
2024-02-11 16:32:46 -0600

    Import initial page-load functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-//alert('Hello Webpack');
+import { contentLoader, headLoader } from "./page-load";
+import "./style.css";
+
+function component(){
+  const header = document.querySelector('header');
+  
+  header.prepend(headLoader());
+
+  const content = document.getElementById('content');
+
+  contentLoader().forEach(e => content.appendChild(e))
+}
+
+
+
+component();
