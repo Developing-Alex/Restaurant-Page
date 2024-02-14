9a789496047ae81a5812679ae64fe76ccea474b1
Alex <alexnowrang@gmail.com>
2024-02-13 20:09:34 -0600

    Refactor code, add image and container

--- src/page-load.js
+++ src/page-load.js
@@ -1,3 +1,5 @@
+import Chef from './pexels-sides-imagery-3351927.jpg'
+
 const headLoader = () => {
   const title = document.createElement('h1');
 
@@ -9,17 +11,30 @@ const headLoader = () => {
 
 const contentLoader = () => {
 
+  const contentContainer = document.createElement('div');
+  contentContainer.id = 'content-container';
+
   const heading = document.createElement('h2');
 
   heading.textContent = 'Best Chicken in America!';
-  heading.id = 'heading'
+  heading.id = 'heading';
   
   const about = document.createElement('p');
 
   about.textContent = `Voted Best Chicken in America three times in a row! We at Bishop's Birdhouse strive to bring you the best finger lickin' chicken experience the country has to offer!`;
   about.id = 'about';
+
+  const chefImage = new Image();
+  chefImage.src = Chef;
+  chefImage.id = 'chef-image';
+
+  const contentArr = [heading, about, chefImage];
+
+  contentArr.forEach(e => contentContainer.appendChild(e));
+
   
-  return [heading, about];
+  
+  return contentContainer;
 }
 
 export { contentLoader, headLoader}
